c3d6e639cb135ee91b0a1bb8cae3b05b4cbd6267
AMilkovski <acakovin@live.com>
2023-07-14 00:14:14 -0400

    Logins story completed

--- src/test/java/com/crm/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/crm/step_definitions/Login_StepDefinitions.java
@@ -7,10 +7,16 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Login_StepDefinitions {
 
     LoginPage loginPage = new LoginPage();
+
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
 
     @Given("user is on the login page of the CRM application")
     public void user_is_on_the_login_page_of_the_crm_application() {
@@ -40,9 +46,25 @@ public class Login_StepDefinitions {
     @Then("user should see that the title of the page is {string}")
     public void user_should_see_that_the_title_of_the_page_is(String expectedTitle) {
 
+        wait.until(ExpectedConditions.titleContains(expectedTitle));
+
         String actualTitle = Driver.getDriver().getTitle();
 
-        Assert.assertTrue(expectedTitle.equals(actualTitle));
+
+        System.out.println(Driver.getDriver().getTitle());
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
     }
+
+
+    @When("user enter {string} in the usernameInputBox")
+    public void user_enter_in_the_username_input_box(String username) {
+      loginPage.username.sendKeys(username);
+    }
+    @When("user enters {string} in the passwordInputBox")
+    public void user_enters_in_the_password_input_box(String password) {
+      loginPage.password.sendKeys(password);
+    }
+
+
 
 }
