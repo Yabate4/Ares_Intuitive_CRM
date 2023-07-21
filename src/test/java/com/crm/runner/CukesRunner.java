8f1290968be76c2db703e8277f18e38ccd43acd9
Cydeo-Huy <hhuylle@gmail.com>
2023-07-20 22:34:26 -0400

    updated B29G7-324 User story

--- src/test/java/com/crm/runner/CukesRunner.java
+++ src/test/java/com/crm/runner/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
 
         dryRun = false,
         publish = true,
-        tags = "@first",
+        tags = "@logout",
         features = "src/test/resources/features",
         glue =  "com/crm/step_definitions"
 
